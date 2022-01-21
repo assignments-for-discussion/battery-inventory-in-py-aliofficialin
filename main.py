0877a92951f30bc18028e971eedc5db0369b42ee
aliofficialin <95541722+aliofficialin@users.noreply.github.com>
2022-01-21 19:24:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-def test_bucketing_by_number_of_cycles():
-  low=[]
-  medium=[]
-  high=[]
-  counts=[]
-  counts=cycles
-  for i in counts(len(counts)):
-  if(counts[i]<400):
-      append(a[i])
-    elif(a[i]<920):
-      append(a[i])
-    else:
-      append(a[i])
-  print("low= "+low)
-  print("count of low cycles is "+len(low))
-  print("medium= "+medium)
-  print("count of medium cycles is "+len(medium))
-  print("high= "+high)
-  print("count of high cycles is "+len(high))
-  print("Done counting :)")
-
-test_bucketing_by_number_of_cycles([100, 300, 500, 600, 900, 1000])
+def count(a):
+    low=[]
+    med=[]
+    high=[]
+    for i in range(len(a)):
+        if a[i]<400:
+            low.append(a[i])
+        elif a[i]>400 and a[i]<919:
+            med.append(a[i])
+        else:
+            high.append(a[i])
+    l=[low,len(low)]
+    m=[med,len(med)]
+    h=[high,len(high)]
+    return l,m,h
+a=[100, 300, 500, 600, 900, 1000]
+print(count(a))
